d9912ae559285e340c0f3a338e94eb027d067e16
jalan116 <jalan116@stu.southtexascollege.edu>
2021-04-22 21:42:46 -0500

    Added NavBar and Auth

---
+++ src/helpers/useFirebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase'
+import { useAuth } from '@vueuse/firebase'
+import 'firebase/auth'
+
+import { firebaseConfig } from '~/config/firebase'
+
+firebase.initializeApp(firebaseConfig)
+
+
+const { auth } = firebase
+const { GoogleAuthProvider } = auth
+
+const { isAuthenticated, user } = useAuth()
+
+export const authentication = () => {
+    const googlePopup = () => auth().signInWithPopup(new GoogleAuthProvider())
+    const signOut = () => auth().signOut()
+    return { googlePopup, signOut, isAuthenticated, user }
+}
+
+export const database = () => {}
