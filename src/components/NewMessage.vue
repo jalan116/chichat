9d78e73623920b745cde27dbbf85b903a73ae607
jalan116 <jalan116@stu.southtexascollege.edu>
2021-04-24 13:06:30 -0500

    Fixed message color

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="absolute bottom-0 left-0 right-0 flex focus-within:ring-4 focus-within:ring-yellow-400">
+      <input type="text" class="flex-grow text-xl font-thin focus:outline-none" />
+      <button class="bg-yellow-600 px-8 focus:outline-none"><mdi:send /></button>
+    </div>
+  </template>
